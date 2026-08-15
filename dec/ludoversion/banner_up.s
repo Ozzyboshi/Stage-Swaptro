16191268ece0dd145d3c44bc51862fb1c9a3606b
Ozzyboshi <gun10137@gmail.com>
2026-08-15 12:32:02 +0200

    ludo version

---
+++ dec/ludoversion/banner_up.s
@@ -0,0 +1,37 @@
+; Bitplane 0:
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 0
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffbb,$ffffffff,$ffffffff,$ddffffff,$ffffffff,$ffffffff,$ffffffff ;row 1
+dc.l $e00009ff,$fd57ffff,$fc3fffff,$fffcff77,$ffff77e3,$c7eeffff,$eefffe0f,$07ffffff,$ffffffff,$ffffffff ;row 2
+dc.l $e4ce41ff,$fd57ffff,$fe7fbbff,$ffe4feef,$ffff33c8,$07ccffe7,$f77fff1f,$8fffffc7,$ffffffff,$ffffffff ;row 3
+dc.l $e49cf3ff,$ffd7ffff,$ffff11ff,$ff24fdde,$3fff11dc,$0b88fff7,$fbbfffbb,$ddffffd7,$ffffffff,$ffffffff ;row 4
+dc.l $e439f3ff,$ffffffff,$fe7fbbff,$ffe4fbbe,$27ff33c8,$13ccfe7e,$7ddffff1,$f8ffffc7,$ffffffff,$ffffffff ;row 5
+dc.l $e00863ff,$ffffffff,$fc3fffff,$fffcf77e,$24ff77e3,$c7eeffbf,$beefffe0,$f07fffff,$ffffffff,$ffffffff ;row 6
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffeeff,$ffffffff,$ffffffff,$ff77ffff,$ffffffff,$ffffffff,$ffffffff ;row 7
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 8; Bitplane 1:
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 0
+dc.l $80000000,$00000000,$00000000,$000001dc,$0000001c,$38000000,$3b800000,$00000000,$00000000,$00000001 ;row 1
+dc.l $9ef7a441,$82a82012,$07c00000,$e00303b8,$6dc08023,$c4010000,$1dc001f0,$0000007c,$0a7df800,$06019f7d ;row 2
+dc.l $92213c48,$32a82812,$03800451,$101b0770,$0dc0c048,$12030018,$0ee000e0,$00000000,$0a456800,$06001145 ;row 3
+dc.l $92420809,$0028aa00,$01000e52,$08db0ee0,$01c0e05c,$020701c9,$c7700044,$00069c54,$0a5d6bff,$000f9145 ;row 4
+dc.l $92840861,$2000aa04,$83800451,$101b1dc0,$0000c048,$120301c1,$c3b8000e,$00065000,$005d0acb,$01801145 ;row 5
+dc.l $9ef7980c,$2003ff84,$87c00000,$e0033b80,$00008023,$c40101c1,$c1dc001f,$0000007c,$007dfbff,$019f9f7d ;row 6
+dc.l $80000000,$00000000,$00000000,$00007700,$0000001c,$38000000,$00ee0000,$00000000,$00000000,$00000001 ;row 7
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 8; Bitplane 2:
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 0
+dc.l $80000000,$00000000,$00000000,$00000198,$00000000,$00000000,$19800000,$00000000,$00000000,$00000001 ;row 1
+dc.l $81085260,$0000001b,$03c00050,$e0030330,$6dc88800,$0011101c,$0cc3e1f0,$f800007c,$a07df800,$36c19e79 ;row 2
+dc.l $8910826c,$0000201b,$01844401,$301b0660,$0dcccc00,$1033301c,$0661c0e0,$700ffc38,$a0450800,$36c01249 ;row 3
+dc.l $8921040d,$80002800,$000eee02,$c8db0cc1,$c1ceee00,$2877701c,$03308844,$2210027c,$00450bff,$000f9249 ;row 4
+dc.l $89420401,$b000aa06,$c1844401,$501b1981,$d80ccc00,$10333181,$81981c0e,$07100238,$00450a01,$0d801249 ;row 5
+dc.l $81000400,$3001ff06,$c3c00050,$e0033301,$db088800,$00111040,$40cc3e1f,$0f8ffc7c,$007dfbff,$0d9f9e79 ;row 6
+dc.l $80000000,$00000000,$00000000,$00006600,$00000000,$00000000,$00660000,$00000000,$00000000,$00000001 ;row 7
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 8; Bitplane 3:
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 0
+dc.l $80000000,$00000000,$00000000,$00000198,$00000000,$00000000,$19800000,$00000000,$00000000,$00000001 ;row 1
+dc.l $80000040,$00000012,$00000000,$e0000330,$0000001c,$38000000,$0cc00000,$0000007c,$007df800,$06019e79 ;row 2
+dc.l $80000048,$00002012,$00000001,$10000660,$00000037,$ec000000,$06600000,$00000000,$00450800,$06001041 ;row 3
+dc.l $80000009,$00002800,$00000002,$08000cc0,$00000023,$d4000000,$03300000,$00000054,$00450bff,$000f9041 ;row 4
+dc.l $80000001,$2000aa04,$80000001,$10001980,$00000037,$ec000000,$01980000,$00000000,$00450a01,$01801041 ;row 5
+dc.l $80000000,$2001ff04,$80000000,$e0003300,$0000001c,$38000000,$00cc0000,$0000007c,$007dfbff,$019f9e79 ;row 6
+dc.l $80000000,$00000000,$00000000,$00006600,$00000000,$00000000,$00660000,$00000000,$00000000,$00000001 ;row 7
+dc.l $ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff,$ffffffff ;row 8
